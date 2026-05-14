177100789c96f1fb5af1bc42aa4381edb7c31de1
nanqurilo-lang <nanqurilo@gmail.com>
2026-05-14 16:06:36 +0530

    fixes

--- src/components/FitnessPros.jsx
+++ src/components/FitnessPros.jsx
@@ -11,14 +11,20 @@ const FitnessPros = () => {
     return (
         <div className='h-full w-full md:py-10 py-4'>
             <h1 className='md:text-5xl sm:text-3xl text-2xl font-bold text-center roboto'>
-                Affiliation And Recognition
+                Affiliation And Recognition 
             </h1>
 
             <div className="max-w-6xl mx-auto grid md:grid-cols-2 gap-10 py-10 px-10">
                 {pros.map((p) => (
                     <div key={p.id} className='h-full w-full flex items-center flex-col gap-6'>
                         <div className='rounded-lg overflow-hidden  w-full'>
-                            <img src={p.scr} alt={p.name} className=' h-[500px] w-full ' />
+                            {/* <img src={p.scr} alt={p.name} className=' h-[500px] w-full ' /> */}
+                            <img
+  src={p.scr}
+  alt={p.name}
+ className='h-[500px] w-full object-cover object-top'
+//className='h-[500px] w-full object-contain'
+/>
                         </div>
                         <div className='flex flex-col items-center'>
                             <h4 className='text-xl font-medium'>{p.name}</h4>
